1125d58567ec2d8a4d8f73961b88fd29f930710f
GracjanHajdasz <hajdaszgracjan@gmail.com>
2026-02-03 20:08:11 +0100

    checking if userexists

--- frontend/src/components/register-page/Register.jsx
+++ frontend/src/components/register-page/Register.jsx
@@ -1,6 +1,9 @@
 import "./Register.css";
+import { useState } from "react"
 
 export default function Login({ setHasAccount, login, setLogin, password, setPassword, users, setUsers }) {
+    const [ isLoginAvailable, setIsLoginAvailable] = useState(true)
+    
     function saveLogin(event) {
         setLogin(event.target.value)
     }
@@ -22,6 +25,17 @@ export default function Login({ setHasAccount, login, setLogin, password, setPas
         setPassword("")
         setHasAccount(true)
     }
+
+    function checkIfUserExists(){
+        const user = users.find( (u)=>login === u.login )
+
+        if (user) {
+            setIsLoginAvailable(false)
+            console.log("user already exists")
+        } else {
+            addUser()
+        }
+    }
     return(
         <div className="login-container">
             <h1>Zarejestruj się</h1>
@@ -30,7 +44,7 @@ export default function Login({ setHasAccount, login, setLogin, password, setPas
                 <input type="password" placeholder="Hasło" onChange={savePassword}/>
             </div>
             <p onClick={() => setHasAccount(true)}>Posiadasz już konto?</p>
-            <button onClick={()=>(addUser())} >Zarejestruj się</button>
+            <button onClick={()=>(checkIfUserExists())} >Zarejestruj się</button>
         </div>
     )
 }
