1324ff8d123dfb8f6e170461c624311c084cf30a
aizey81 <igozaur05@gmail.com>
2026-02-06 22:28:27 +0100

    auth controllers errors updated

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -6,16 +6,18 @@ exports.register = async (req, res) => {
 
         const userExists = await db.query('SELECT * FROM users WHERE username = $1', [username]);
         if(userExists.rows.length > 0) {
-            return res.status(400).json({ message: 'Użytkownik jest już zarejestrowany!'});
+            return res.status(400).json({ message: 'User already registered'});
         }
 
         await db.query(
             'INSERT INTO users (username, password) VALUES ($1, $2)', 
             [username, password]);
 
+        res.status(201).json({ message: 'User registered successfully'});
+
     } catch (err) {
         console.error(err.message);
-        res.status(500).json({ message: 'Bład podczas rejestracji!'});
+        res.status(500).json({ message: 'Error during registration'});
     }
 }
 
@@ -27,20 +29,25 @@ exports.login = async( req, res) => {
             'SELECT * FROM users WHERE username = $1 and password = $2', 
             [username, password])
 
+        if (result.rows.length === 0) {
+            return res.status(401).json({ message: "Incorrect username or password"});
+        }
+
         const user = result.rows[0];
 
         if (result.rows.length > 0) {
-            res.json({ message: "Login przebiegło pomyślnie!", user: result.rows[0] });
+            res.json({ message: "User logged in sucessfully", 
+            user: { 
+                id: user.id, 
+                username: user.username 
+            }
+            });
         } else {
-            res.status(401).json({ message: "Nieprawidłowe dane logowania!"});
+            res.status(401).json({ message: "Incorrect username or password"});
         }
-
-        // if(!user) {
-        //     return res.status(400).json({ message: 'User does not exist!'});
-        // }
 
     } catch (err) {
         console.error(err.message);
-        res.status(500).json({ message: 'Bład podczas logowania!'});
+        res.status(500).json({ message: 'Error during login'});
     }
 }
