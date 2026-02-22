06adad3ebaee9cbf64f810712c921a23e03e19cd
HubertGorny <hubertgornydata@gmail.com>
2026-02-22 20:53:05 +0100

    Exam questions delivery
    Basicly for B category, endpoint does not accept any additional data for now

--- Backend Py/prawkojazdy.py
+++ Backend Py/prawkojazdy.py
@@ -3,6 +3,7 @@ from psycopg_pool import ConnectionPool
 from contextlib import asynccontextmanager
 from datetime import datetime
 from fastapi import FastAPI
+from fastapi import Request
 from zoneinfo import ZoneInfo
 from psycopg.errors import UniqueViolation
 from psycopg import OperationalError, InterfaceError, DatabaseError, Error
@@ -13,6 +14,12 @@ from pathlib import Path
 from dotenv import load_dotenv
 import uvicorn
 import logging
+import pandas as pd
+import random as rd
+from psycopg.rows import dict_row
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
+
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
@@ -23,6 +30,7 @@ logger = logging.getLogger("prawkojazdy")
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 def base_dir():
     return Path(sys.executable).resolve().parent if getattr(sys, "frozen", False) else Path(__file__).resolve().parent
+
 load_dotenv(base_dir() / ".env", override=True)
 DATABASE_URL = os.getenv("DATABASE_URL")
 HOST = os.getenv("HOST", "127.0.0.1")
@@ -30,7 +38,9 @@ PORT = int(os.getenv("PORT", "8000"))
 
 if not DATABASE_URL:
     raise RuntimeError("Brak DATABASE_URL. Dodaj plik .env")
+
 pool = ConnectionPool(conninfo = DATABASE_URL, min_size=1, max_size=10, timeout=10)
+
 @asynccontextmanager
 async def lifespan(app:FastAPI):
     logger.info("API startup - opening DB pool")
@@ -38,18 +48,27 @@ async def lifespan(app:FastAPI):
     yield
     logger.info("API startup - closing DB pool")
     pool.close()
+
 app = FastAPI(lifespan=lifespan)
+
+BASE_DIR = Path(__file__).resolve().parent
+MEDIA_DIR = BASE_DIR / "pytania_egzaminacyjne_2025"
+
+app.mount("/media", StaticFiles(directory=MEDIA_DIR), name = "media")
+
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
 def verify_password(password: str, password_hash: str) -> bool:
     return pwd_context.verify(password, password_hash)
+
 def error_db(error : Exception):
       if isinstance(error,(OperationalError, InterfaceError)):
             raise HTTPException(status_code=503, detail = "Baza danych niedostępna")
       elif isinstance(error, DatabaseError):
             raise HTTPException(status_code=500, detail=f"Błąd bazy danych: {error.__class__.__name__}")
       raise HTTPException(status_code=500, detail="Nieznany błąd serwera")
+
 @app.post("/register")
 def register(data : dict):
     clientid = data["clientid"]
@@ -69,6 +88,7 @@ def register(data : dict):
         logger.exception("Error in /register clientid=%s", clientid)
         conn.rollback()
         error_db(e)
+
 @app.post("/login")
 def login(data : dict):
      clientid = data["clientid"]
@@ -87,6 +107,7 @@ def login(data : dict):
      except Exception as e:
         logger.exception("Error in /login clientid=%s", clientid)
         error_db(e)
+
 @app.post("/editBookings")
 def editBookings(data : dict):
      tabela_dat = data["data"]
@@ -104,6 +125,7 @@ def editBookings(data : dict):
      except Exception as e:
         conn.rollback()
         error_db(e)
+
 @app.post("/getBookings")
 def getBookings(data:dict):
     data_ = data["data"]
@@ -115,5 +137,45 @@ def getBookings(data:dict):
             return {"Msg": [{"data": dt.strftime("%Y-%m-%d %H:%M:%S"),"status": "available" if not zajete else "booked"}for dt, zajete in rows]}
     except Exception as e:
         error_db(e)
+
+@app.post("/getExam")
+def getExam(request : Request):
+    try:
+        with pool.connection() as conn:
+            with conn.cursor(row_factory = dict_row) as cur:
+                cur.execute("Select pytanie, poprawna_odp, media, liczba_punktów from pytaniaegzaminacyjne where czykatb = TRUE and zakres_struktury = %s", ("PODSTAWOWY",))
+                rows = cur.fetchall()
+    except Exception as e:
+        error_db(e)
+
+    df1 = pd.DataFrame(rows)
+    PODSTAWOWE_3pkt = df1[df1["liczba_punktów"] == 3].sample(n=10)
+    PODSTAWOWE_2pkt = df1[df1["liczba_punktów"] == 2].sample(n=6)
+    PODSTAWOWE_1pkt = df1[df1["liczba_punktów"] == 1].sample(n=4)
+    PODSTAWOWE = pd.concat([PODSTAWOWE_1pkt, PODSTAWOWE_2pkt,PODSTAWOWE_3pkt], ignore_index=True)
+    base_url = str(request.base_url) + "media/"
+    PODSTAWOWE["media"] =  base_url + PODSTAWOWE["media"].astype(str)
+    PODSTAWOWE = PODSTAWOWE.sample(frac=1).reset_index(drop=True)
+
+    try:
+        with pool.connection() as conn:
+            with conn.cursor(row_factory = dict_row) as cur:
+                cur.execute("Select pytanie,odpowiedź_a,odpowiedź_b,odpowiedź_c,poprawna_odp, media, liczba_punktów from pytaniaegzaminacyjne where czykatb = TRUE and zakres_struktury = %s",("SPECJALISTYCZNY",))
+                rows = cur.fetchall()
+    except Exception as e:
+        error_db(e)
+
+    df1 = pd.DataFrame(rows)
+    SPECJALISTYCZNE_3pkt = df1[df1["liczba_punktów"] == 3].sample(n=6)
+    SPECJALISTYCZNE_2pkt = df1[df1["liczba_punktów"] == 2].sample(n=4)
+    SPECJALISTYCZNE_1pkt = df1[df1["liczba_punktów"] == 1].sample(n=2)
+    SPECJALISTYCZNE = pd.concat([SPECJALISTYCZNE_1pkt,SPECJALISTYCZNE_2pkt,SPECJALISTYCZNE_3pkt], ignore_index=True)
+    SPECJALISTYCZNE["media"] =  base_url + SPECJALISTYCZNE["media"].astype(str) 
+    SPECJALISTYCZNE = SPECJALISTYCZNE.sample(frac=1).reset_index(drop=True)
+
+    return {"podstawowe": PODSTAWOWE.to_dict(orient="records"),
+            "specjalistyczne" : SPECJALISTYCZNE.to_dict(orient="records")
+            }
+
 if __name__ == "__main__":
     uvicorn.run(app, host=HOST, port=PORT)
