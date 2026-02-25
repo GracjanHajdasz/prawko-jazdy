6faaf9beb4c1c9ec777b5c45a3f80d81c10da4e7
GracjanHajdasz <hajdaszgracjan@gmail.com>
2026-02-25 17:48:45 +0100

    exam loading component

--- frontend/src/components/exam/Exam.jsx
+++ frontend/src/components/exam/Exam.jsx
@@ -2,21 +2,25 @@ import "./Exam.css";
 import ExamResults from "../exam-results/ExamResults";
 import ExamLeftPanel from "./ExamLeftPanel";
 import ExamRightPanel from "./ExamRightPanel";
+import ExamLoading from "./exam-loading/ExamLoading.jsx";
 import { useExam } from "./UseExam";
 import { formatTime, calculateScore } from "./ExamUtils";
+import { useState } from "react";
 
 export default function Exam() {
+  const [isLoaded, setIsLoaded] = useState(false); //do loading screena
   const examState = useExam();
-  const { loading, questions, isExamFinished, answers, examTimeLeft } = examState;
+  const { loading, questions, isExamFinished, answers, examTimeLeft } =
+    examState;
 
-  if (loading || questions.length === 0) return <div>Ładowanie...</div>;
+  //if (loading || questions.length === 0) return <div>Ładowanie...</div>;
 
   if (isExamFinished) {
     const results = calculateScore(questions, answers);
     const czasEgzaminuSekundy = 1500 - examTimeLeft;
 
     return (
-      <ExamResults 
+      <ExamResults
         sumaPunktow={results.suma}
         punktyPodstawowe={results.podstawowe}
         punktySpecjalistyczne={results.specjalistyczne}
@@ -25,10 +29,12 @@ export default function Exam() {
     );
   }
 
-  return (
+  return isLoaded ? (
     <div className="exam-container">
       <ExamLeftPanel examState={examState} />
       <ExamRightPanel examState={examState} />
     </div>
+  ) : (
+    <ExamLoading setIsLoaded={setIsLoaded} />
   );
 }
