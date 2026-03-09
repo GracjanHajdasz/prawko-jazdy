11cc9978accdd25e416597c4ed2a61512d3a2591
GracjanHajdasz <hajdaszgracjan@gmail.com>
2026-03-09 23:01:38 +0100

    basic html structure for admin panel

--- frontend/src/components/admin-panel/AdminPanel.jsx
+++ frontend/src/components/admin-panel/AdminPanel.jsx
@@ -1,5 +1,18 @@
-import "./AdminPanel.jsx";
+import "./AdminPanel.css";
 
 export default function AdminPanel() {
-  return <p>Admin panel</p>;
+  return (
+    <div className="admin-panel-container">
+      <aside className="admin-panel-sidebar">
+        <h2>Panel administratora</h2>
+        <nav className="admin-panel-nav">
+          <button>Kursanci</button>
+          <button>Instruktorzy</button>
+          <button>Kalendarz</button>
+          <button>Faktury</button>
+        </nav>
+      </aside>
+      <main className="admin-panel-content"></main>
+    </div>
+  );
 }
